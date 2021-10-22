d7e6c6ebccc11c2e117cf6b20176f45636d885e1
muthu <mkandanmk7@gmail.com>
2021-10-22 16:32:12 +0530

    intialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import SearchBar from "./components/Search/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <h2>Home page</h2>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,13 @@
+// import { Search } from "@mui/icons-material";
+import React from "react";
+// import  {Search} from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+function SearchBar() {
+  return (
+    <div>
+      <SearchIcon />
+    </div>
+  );
+}
+
+export default SearchBar;
